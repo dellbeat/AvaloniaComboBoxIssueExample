5dc75acb2489bb9eb523fda082b4ba9ede7f4925
dellbeat <daizhicai@foxmail.com>
2023-04-13 18:57:50 +0800

    Upload a comboBox issue example

---
+++ ComboBoxExample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace CommoBoxExample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ComboBoxExample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CommoBoxExample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
